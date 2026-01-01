f0fef6dd45fc2004046a861da51c676a759cf590
olana16 <oligeda16@gmail.com>
2026-01-01 11:27:07 +0300

    mongodb connection created

---
+++ server/index.js
@@ -0,0 +1,21 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+
+const CONNECTION_URL = 'mongodb+srv://olana16:olana16123@cluster0.ft009ui.mongodb.net/?appName=Cluster0';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL)
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
